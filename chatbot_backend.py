ea18874355fda3050db9c185238402ace2548620
EngrHashim160 <engrhashim160@gmail.com>
2025-08-04 17:13:39 +0500

    chatbot completed

--- chatbot_backend.py
+++ chatbot_backend.py
@@ -1,37 +1,30 @@
 from langgraph.graph import StateGraph, START, END
-from langchain_groq import ChatGroq
-from typing import TypedDict, Literal, Annotated
-from pydantic import BaseModel, Field
-from dotenv import load_dotenv
-from langchain_core.messages import SystemMessage, HumanMessage, BaseMessage
-
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage
+from langchain_openai import ChatOpenAI
+from langgraph.checkpoint.memory import InMemorySaver
 from langgraph.graph.message import add_messages
-from langgraph.checkpoint.memory import MemorySaver
+from langchain_groq import ChatGroq 
+from dotenv import load_dotenv
 
 load_dotenv()
 
-class ChatState(TypedDict):
-    messages: Annotated[list[BaseMessage], add_messages]
-    
 llm = ChatGroq(model="Gemma2-9b-It",temperature=0)
 
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
 def chat_node(state: ChatState):
-    # take the user query from state
     messages = state['messages']
-    
-    # send to llm
     response = llm.invoke(messages)
-    
-    # response store state
-    return {
-        'messages': [response]
-    }
-    
-checkpointer = MemorySaver()
+    return {"messages": [response]}
+
+# Checkpointer
+checkpointer = InMemorySaver()
+
 graph = StateGraph(ChatState)
+graph.add_node("chat_node", chat_node)
+graph.add_edge(START, "chat_node")
+graph.add_edge("chat_node", END)
 
-graph.add_node('chat_node', chat_node)
-
-graph.add_edge(START, 'chat_node')
-graph.add_edge('chat_node', END)
-workflow = graph.compile(checkpointer=checkpointer)
+chatbot = graph.compile(checkpointer=checkpointer)
